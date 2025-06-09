e6fc04cff92a30d95f89d1753e103782ae7b7e48
Alejandro Palomino <axp22a@acu.edu>
2025-06-09 16:39:05 -0600

    Chapter 12

--- MobileDev/Assets/Scripts/MainMenuBehaviour.cs
+++ MobileDev/Assets/Scripts/MainMenuBehaviour.cs
@@ -9,7 +9,8 @@ public class MainMenuBehaviour : MonoBehaviour
     /// </summary>
     /// <param name="levelName">The name of the level
     /// we want to go to</param>
-    /// 
+    public GameObject controlPanel;
+
     public TextMeshProUGUI highScoreText;
     //public GameObject controlPanel; // use this in chapter 12
 
@@ -27,8 +28,14 @@ public class MainMenuBehaviour : MonoBehaviour
 
     public void Start()
     {
+        /* Unpause the game if needed */
+        Time.timeScale = 1;
+
         // check for a high score and set it to our TMProUGUI
         GetAndDisplayScore();
+
+        // slide in the Panel with all the goodies
+        SlideMenuIn(controlPanel);
 
     }
 
@@ -37,4 +44,51 @@ public class MainMenuBehaviour : MonoBehaviour
         highScoreText.text = "High Score: " + PlayerPrefs.GetInt("score").ToString();
     }
 
+    /// <summary>
+    /// Will move an object from the left side of the screen
+    /// to the center
+    /// /// </summary>
+    /// <param name="obj">The UI element we would like to
+    /// move</param>
+    public void SlideMenuIn(GameObject obj)
+    {
+        obj.SetActive(true);
+        var rt = obj.GetComponent<RectTransform>();
+        if (rt)
+        {
+            /* Set the object's position offscreen */
+            var pos = rt.position;
+            pos.x = -Screen.width / 2;
+            rt.position = pos;
+            
+            /* Move the object to the center of the screen
+            (x of 0 is centered) */
+            var tween = LeanTween.moveX(rt, 0, 1.5f);
+            tween.setEase(LeanTweenType.easeInOutExpo);
+            tween.setIgnoreTimeScale(true);
+        }
+    }
+
+    /// <summary>
+    /// Will move an object to the right offscreen
+    /// </summary>
+    /// <param name="obj">The UI element we would like to
+    /// move </param>
+    public void SlideMenuOut(GameObject obj)
+    {
+        var rt = obj.GetComponent<RectTransform>();
+        if (rt)
+        {
+            var tween = LeanTween.moveX(rt,
+            Screen.width / 2, 0.5f);
+
+            tween.setEase(LeanTweenType.easeOutQuad);
+            tween.setIgnoreTimeScale(true);
+            
+            tween.setOnComplete(() =>
+            {
+                obj.SetActive(false);
+            });
+        }
+    }
 }
